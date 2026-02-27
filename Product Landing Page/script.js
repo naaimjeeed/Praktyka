0e0b1fc449dfdbb96952491b1330df6aecacefc3
naaimjeeed <jiegieskier@gmail.com>
2026-02-27 14:27:03 +0100

    css refresh

--- Product Landing Page/script.js
+++ Product Landing Page/script.js
@@ -1,7 +1,20 @@
 const navbar = document.getElementById("nav-bar");
 const hamburgerMenu = document.getElementById("hamburger-menu");
+const header = document.getElementById("header");
+const signIn = document.getElementById("sign-in");
 
 hamburgerMenu.addEventListener("click", () =>{
     navbar.classList.toggle("active");
 });
 
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+signIn.addEventListener("click", () => {
+    window.location.href = "#main-email";
+})
